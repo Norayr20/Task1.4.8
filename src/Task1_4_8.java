071d6ec31542d0892fe1a290499287cf43b52672
Norayr <asoyannorayr99@gmail.com>
2023-10-29 17:05:11 +0300

    add

--- src/Task1_4_8.java
+++ src/Task1_4_8.java
@@ -3,15 +3,10 @@ public class Task1_4_8 {
     public static void main(String[] args) {
 
         System.out.println(factorial(1));
-
     }
 
     public static BigInteger factorial(int value) {
-        if (value <= 1) {
-            return BigInteger.valueOf(1);
-        } else {
-            BigInteger bigInteger = BigInteger.valueOf(value * (value - 1));
-            return bigInteger;
+        if (value <= 1) return BigInteger.ONE;
+        return BigInteger.valueOf(value).multiply(factorial(value - 1));
         }
     }
-}
