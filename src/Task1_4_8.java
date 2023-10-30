64b9c276b260f8515b3317f9b0e943d47c050af8
Norayr <asoyannorayr99@gmail.com>
2023-10-30 19:59:21 +0300

    add

--- src/Task1_4_8.java
+++ src/Task1_4_8.java
@@ -2,11 +2,14 @@ import java.math.BigInteger;
 public class Task1_4_8 {
     public static void main(String[] args) {
 
-        System.out.println(factorial(1));
+        System.out.println(factorial(3));
     }
 
     public static BigInteger factorial(int value) {
-        if (value <= 1) return BigInteger.ONE;
-        return BigInteger.valueOf(value).multiply(factorial(value - 1));
+        if (value <= 1) {
+            return BigInteger.valueOf(1);
+        } else {
+            return BigInteger.valueOf(value).multiply(factorial(value - 1));
         }
     }
+}
